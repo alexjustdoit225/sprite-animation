5908ec3d81f088fd7cb7e36e622244bfa38fc1d0
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-19 13:15:40 -0600

    Update script.js
    -added drawImage for sprite sheet

--- script.js
+++ script.js
@@ -8,13 +8,21 @@ console.log(c);
 //create an image, I'm using this to store the anitmated sprite sheet
 const playerImage = new Image(); 
 playerImage.src = '/assets/shadow_dog.png'; 
+const spriteWidth = 520; 
+const spriteHeight = 523;
+let frameX = 0; 
+let frameY = 0; 
 
 function animate(){
     //used to clear the canvas 
     c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
     //draw
-    fillRect(50,50,100,100); 
+    c.fillRect(50,50,100,100);
+    //drawImage draws the sprite animation we stored; first argument is the sprite sheet, x and y coordinates, width and height
+    //c.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh); all the s's represent the cutout portion of the sprite sheet
+    c.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);  
     //runs animation loop passed 
     requestAnimationFrame(animate); 
-
-}
+};
+//don't forget to call the animate function 
+animate(); 
