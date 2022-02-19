2e36005337c93bd87cdb9c85e6331c1b099c30a9
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-19 12:48:28 -0600

    Update script.js
    -animation loop setup

--- script.js
+++ script.js
@@ -8,3 +8,13 @@ console.log(c);
 //create an image, I'm using this to store the anitmated sprite sheet
 const playerImage = new Image(); 
 playerImage.src = '/assets/shadow_dog.png'; 
+
+function animate(){
+    //used to clear the canvas 
+    c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
+    //draw
+    fillRect(50,50,100,100); 
+    //runs animation loop passed 
+    requestAnimationFrame(animate); 
+
+}
