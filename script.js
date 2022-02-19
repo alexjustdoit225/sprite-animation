5f5e6a4c8cdc5ebff54ccce1edc9b2f0282ff9d0
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-19 12:43:21 -0600

    updated setup js
    -added sprite sheet

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const canvas = document.getElementById('canvas1'); 
 const c  = canvas.getContext('2d'); 
+const CANVAS_WIDTH = canvas.width = 600; 
+const CANVAS_HEIGHT = canvas.height = 600; 
+//see the content of getContext('2d')
+console.log(c);
 
+//create an image, I'm using this to store the anitmated sprite sheet
+const playerImage = new Image(); 
+playerImage.src = '/assets/shadow_dog.png'; 
