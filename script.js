9c010c234751fe495d930d9101f18c1dcb178e88
alexjustdoit225 <alex.minnie.aw@gmail.com>
2022-02-20 15:47:12 -0600

    Update script.js

--- script.js
+++ script.js
@@ -10,28 +10,78 @@ const playerImage = new Image();
 playerImage.src = '/assets/shadow_dog.png'; 
 const spriteWidth = 575; 
 const spriteHeight = 523;
-let frameX = 0; 
-let frameY = 0; 
+let playerState = 'run'; 
 //gameFrame to control speed 
 let gameFrame = 0; 
-const staggerFrames = 6; 
-
+const staggerFrames = 5; 
+const spriteAnimations = []; 
+const animationStates = [
+    {
+        name: 'idle',
+        frames: 7, 
+    }, 
+    {
+        name: 'jump', 
+        frames: 7,
+    },
+    {
+        name: 'fall', 
+        frames: 7,
+    },
+    {
+        name: 'run',
+        frames: 9, 
+    },
+    {
+        name: 'dizzy', 
+        frames: 11, 
+    }, 
+    {
+        name: 'sit', 
+        frames: 5, 
+    },
+    {
+        name: 'roll',
+        frames: 7, 
+    }, 
+    {
+        name: 'bite', 
+        frames: 7, 
+    }, 
+    {
+        name: 'ko', 
+        frames: 12, 
+    },
+    {
+        name: 'getHit',
+        frames: 4, 
+    }
+]; 
+animationStates.forEach((state, index) => {
+    let frames = {
+        loc: [], 
+    }
+    for (let j = 0; j < state.frames; j++){
+        let positionX = j * spriteWidth; 
+        let positionY = index * spriteHeight; 
+        frames.loc.push({x: positionX, y: positionY});
+    }
+    spriteAnimations[state.name] = frames; 
+});
+console.log(animationStates); 
 
 function animate(){
     //used to clear the canvas 
     c.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT); 
-    //draw
-    //c.fillRect(50,50,100,100);
+    //advanced js
+    let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['idle'].loc.length;
+    let frameX = spriteWidth * position; 
+    let frameY = spriteAnimations[playerState].loc[position].y; 
     //drawImage draws the sprite animation we stored; first argument is the sprite sheet, x and y coordinates, width and height
     //c.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh); all the s's represent the cutout portion of the sprite sheet
-    c.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, 
+    c.drawImage(playerImage, frameX, frameY, 
     spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);  
     //gives player resting animation; goes horizontally along sprite sheet
-    if(gameFrame % staggerFrames == 0){
-        if (frameX < 6) frameX++; 
-        else frameX = 0;
-    }
-    
     gameFrame++; 
     //runs animation loop passed 
     requestAnimationFrame(animate); 
